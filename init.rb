a1015813d8ac67be663b5482baf5c9060efd20e4
Onur Küçük <onur@delipenguen.net>
2016-08-02 12:58:05 +0300

    add newline to view partial, increment version number

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_uxta do
   name 'Redmine UXTA plugin'
   author 'Onur Kucuk'
   description 'UI enhancements needed to be done programmatically'
-  version '0.5.0'
+  version '0.5.1'
   url 'http://www.ozguryazilim.com.tr'
   author_url 'http://www.ozguryazilim.com.tr'
   requires_redmine :version_or_higher => '3.0.3'
